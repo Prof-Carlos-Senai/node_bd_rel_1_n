0ac2b6aa0d69246258deb294f5d2808f458aaaf2
Carlos <silva@edu.sc.senai.br>
2024-08-05 09:39:52 -0300

    fazendo o atualizar

--- index.js
+++ index.js
@@ -45,10 +45,21 @@ app.get('/fabricante', async (req,res)=>{
             res.status(200).json(pesq.marca)
         }
     }catch(err){
-        console.error('Não foi possível consultar os dados!')
+        console.error('Não foi possível consultar os dados!',err)
         res.status(500).json({message: "Não foi possível consultar os dados!"})
     }
     
+})
+
+app.get('/fabricantes', async (req,res)=>{
+    try{
+        const pesq = await Fabricante.findAll()
+        console.log(pesq)
+        res.status(200).json(pesq)
+    }catch(err){
+        console.error('Não foi possível listar os dados!',err)
+        res.status(500).json({message: "Não foi possível listar os dados!"})
+    }
 })
 
 app.post('/fabricante', async (req,res)=>{
@@ -60,17 +71,36 @@ app.post('/fabricante', async (req,res)=>{
         console.log(pesq)
         res.status(201).json(pesq)
     }catch(err){
-        console.error('Não foi possível gravar os dados!')
+        console.error('Não foi possível gravar os dados!',err)
         res.status(500).json({message: "Não foi possível gravar os dados!"})
     }
 })
 
-app.delete('/fabricante/:id', (req,res)=>{
+app.delete('/fabricante/:id', async (req,res)=>{
     const valor = req.params
-    console.log(valor)
-    console.log('---------------')
-    console.log(valor.id)
-    res.status(200).json({message: "dados recebidos"})
+    // console.log(valor)
+    const pesq = await Fabricante.findByPk(valor.id)
+    try{
+        if(pesq === null){
+            console.log(pesq)
+            res.status(404).json({message: "Fabricante inexistente no sistema!"})
+        }else{
+            console.log(pesq)
+            await Fabricante.destroy({where: { codFabricante: valor.id}})
+            res.status(200).json({message: "Fabricante excluído!"})
+        }
+    }catch(err){
+        console.error('Não foi possível apagar os dados!',err)
+        res.status(500).json({message: "Não foi possível apagar os dados!"})
+    }   
+})
+
+app.put('/fabricante', async (req,res)=>{
+    const valores = req.body
+    console.log(valores)
+    const pesq = await Fabricante.findByPk(valores.codFabricante)
+    console.log(pesq)
+    res.status(200).json(pesq)
 })
 
 app.get('/', (req,res)=>{
